2a040f964eed6ab43402ea0a531dfe93ca44b83b
syed mohammed sazid ullah <mdsazid9089@gmail.com>
2025-08-17 01:15:01 +0600

    added shop section and waiting for collaboration

---
+++ src/components/Shops/ShopsSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ShopsSection = () => {
+  return (
+    <section className="shops-section">
+      <h2>Shops</h2>
+      <div className="shops-list">
+      </div>
+    </section>
+  );
+};
+
+export default ShopsSection;
