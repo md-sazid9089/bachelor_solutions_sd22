c6ec00480a1e6478e1c38de8c8c7ed33230cb14e
syed mohammed sazid ullah <mdsazid9089@gmail.com>
2025-08-17 01:34:53 +0600

    added subscription

---
+++ src/components/Subscription/SubscriptionSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SubscriptionSection = () => {
+  return (
+    <section>
+      <h2>Subscribe to our Newsletter</h2>
+      <button>Subscribe</button>
+    </section>
+  );
+};
+
+export default SubscriptionSection;
