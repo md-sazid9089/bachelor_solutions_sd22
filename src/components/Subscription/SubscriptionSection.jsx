db70cde20186c43f94ec5b5791550a96d599dd7a
syed mohammed sazid ullah <mdsazid9089@gmail.com>
2025-08-17 02:20:38 +0600

    resolving

--- src/components/Subscription/SubscriptionSection.jsx
+++ src/components/Subscription/SubscriptionSection.jsx
@@ -375,15 +375,3 @@ const SubscriptionSection = () => {
 };
 
 export default SubscriptionSection;
-import React from 'react';
-
-const SubscriptionSection = () => {
-  return (
-    <section>
-      <h2>Subscribe to our Newsletter</h2>
-      <button>Subscribe</button>
-    </section>
-  );
-};
-
-export default SubscriptionSection;
